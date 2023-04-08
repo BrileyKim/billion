99fef89951ebb6bfc60554f24c73b2fef771cb85
BrileyKim <briley.kim.lee@gmail.com>
2023-04-08 20:59:26 +0900

    [김가남] 로그인 성공시 대시보드에 사용자 정보 출력

--- src/main/java/com/ganam/billion/config/MyUserDetailService.java
+++ src/main/java/com/ganam/billion/config/MyUserDetailService.java
@@ -26,11 +26,14 @@ public class MyUserDetailService implements UserDetailsService {
         Optional<Setting> phone= settingService.findSettingValue("phone");
         Setting phoneSetting = phone.orElseThrow(() -> new UsernameNotFoundException("전화번호를 다시 입력해주세요."));
 
+        Optional<Setting> nickname = settingService.findSettingValue("nickname");
+        Setting nameSetting = nickname.orElseThrow(() -> new UsernameNotFoundException("부적절한 닉네임입니다."));
+
         Optional<Setting> password = settingService.findSettingValue("password");
         Setting passwordSetting = password.orElseThrow(() -> new PasswordExpiredException("비밀번호를 다시 입력해주세요."));
 
         return User.builder()
-                .username(phoneSetting.getSetting_value())
+                .username(nameSetting.getSetting_value())
                 .password(passwordSetting.getSetting_value())
                 .roles("관리자")
                 .build();
--- src/main/java/com/ganam/billion/controller/ViewController.java
+++ src/main/java/com/ganam/billion/controller/ViewController.java
@@ -1,8 +1,11 @@
 package com.ganam.billion.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/view")
@@ -14,7 +17,11 @@ public class ViewController {
     }
 
     @GetMapping("/dashboard")
-    public String dashboardPage(){return "view/dashboard";}
+    public String dashboardPage(Principal principal, Model model){
+        model.addAttribute("loginId",principal.getName());
+
+        return "view/dashboard";
+    }
 
     @GetMapping("/join")
     public String joinPage(){ return "view/join";}
